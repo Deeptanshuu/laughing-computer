7024f54d8fdd5e8902a506a20e9f67f4f1c4652b
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2024-05-18 22:51:55 +0530

    copy to cb

--- Client/src/Components/Showcase.jsx
+++ Client/src/Components/Showcase.jsx
@@ -42,6 +42,19 @@ const Showcase = () => {
   const shareUrl = `https://tsukimarket.vercel.app/shop/product/showcase?id=${selectedItem.id}`;
   const title = 'Hey! Check out my Mini Project\n';
 
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(shareUrl);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+      toast.success("Link Copied Successfully");
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  };
+
   const handleImageClick = (imgSrc) => {
     window.scrollTo(0, 320);
     setMainImg(imgSrc);
@@ -268,6 +281,14 @@ const Showcase = () => {
         >
           <EmailIcon size={54} round={false}  />
         </EmailShareButton>
+
+        <button className="btn btn-outline border-0" onClick={handleCopy}>
+          <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M21 8C21 6.34315 19.6569 5 18 5H10C8.34315 5 7 6.34315 7 8V20C7 21.6569 8.34315 23 10 23H18C19.6569 23 21 21.6569 21 20V8ZM19 8C19 7.44772 18.5523 7 18 7H10C9.44772 7 9 7.44772 9 8V20C9 20.5523 9.44772 21 10 21H18C18.5523 21 19 20.5523 19 20V8Z" fill="#0F0F0F"/>
+          <path d="M6 3H16C16.5523 3 17 2.55228 17 2C17 1.44772 16.5523 1 16 1H6C4.34315 1 3 2.34315 3 4V18C3 18.5523 3.44772 19 4 19C4.55228 19 5 18.5523 5 18V4C5 3.44772 5.44772 3 6 3Z" fill="#0F0F0F"/>
+          </svg>
+        </button>
+
         </div>
 
           </div>
